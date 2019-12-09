0821308bd769ee1192548b2806fe1fecb0d13fd2
대전 <show0389@naver.com>
2019-12-09 23:44:40 +0900

    집에서

--- src/main/java/com/now/freeboard/dao/IFreeBoardDao.java
+++ src/main/java/com/now/freeboard/dao/IFreeBoardDao.java
@@ -11,11 +11,31 @@ public interface IFreeBoardDao {
 	 * 자유게시판 전체 검색
 	 * @return
 	 */
-	public List<FreeBoardVO> selectFreeBoadrList();
+	public List<FreeBoardVO> selectFreeBoardList(FreeBoardVO freeVO);
 	
 	/**
-	 * 자유게시판 등록
+	 * 자유게시판 상세정보
+	 * @return
+	 */
+	public FreeBoardVO selectFreeBoard(int fr_no);
+	
+	/**
+	 * 자유게시판 수정
 	 */
 	public int updateFreeBoard(FreeBoardVO freeVO);
 	
+	
+	/**
+	 * 자유게시판 등록
+	 * @param freeVO
+	 * @return
+	 */
+	public int insertFreeBoard(FreeBoardVO freeVO);
+	
+	/**
+	 * 자유게시판 삭제
+	 * @param bo_no
+	 * @return
+	 */
+	public int deleteFreeBoard(int fr_no);
 }
